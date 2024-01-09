6a2ea0ea7b7b47c79770c26fd5143bbaec1bbd25
AresChris <148880863+AresChris@users.noreply.github.com>
2024-01-09 11:08:30 +0100

    maj diagramme

--- Algo/Java/Procedures_fonctions/exercice_5/src/exercice_5/App.java
+++ Algo/Java/Procedures_fonctions/exercice_5/src/exercice_5/App.java
@@ -4,26 +4,28 @@ import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class App {
-	public static void month(String longMonth; String shortMonth)
+	public static void month(int longMonth, int shortMonth)
 	{
-		char[] thirtyOne = ["Janvier", "mars", "mai", "Juillet", "Août", "Octobre","decembre"];
-		char[] thirty = ["Avril", "Juin", "Septembre", "Novembre"];
+		int[] thirtyOne = {1, 3, 5, 7, 8, 10,12};
+		int[] thirty = {4, 6, 9, 11};
 		boolean monthThirty = true;
 		boolean monthThirtyOne = true;
+		int month = 0;
+		Scanner sc = new Scanner(System.in);
 
 		//initialisation de la booleenne monthThirtyOne (jours à 31)
-		if(day <=31 || day>=1) {
+		if(month <=31 || month>=1) {
 			monthThirtyOne = true;
 		}
-		else if (day >31 || day <1)
+		else if (month >31 || month <1)
 		{
 			monthThirtyOne = false;
 		}
 			//initialisation de la booleenne monthThiry (jours à 30)
-			if (day >30 || day <1){
+			if (month >30 || month <1){
 				monthThirty = false;
 			}
-			else if (day<=30 || day >=1)
+			else if (month<=30 || month >=1)
 			{
 				monthThirty = true;
 			}
@@ -32,30 +34,31 @@ public class App {
 			boolean bissextile = (annee%4 == 0 && annee %100 != 0) || annee% 400 == 0; 	// booléenne bissextile = true
 			boolean jourValide = true;
 			DecimalFormat formater = new DecimalFormat("00"); // Formater à deux nombres
+			month(0, 0);
 			
+			Scanner sc = new Scanner(System.in);
+
 			// initialisation de la boooleenne 'jourValide'
-			if (jour>31 || mois>12 || mois<1 || !trueMonth) {
+			if (jour>31 || mois>12 || mois<1 || !jourValide) {
 					jourValide = false;
-			} else if (jour>=1 && jour<=29 && mois == 2 && bissextile || trueMonth) {
+			} else if (jour>=1 && jour<=29 && mois == 2 && bissextile || jourValide) {
 					jourValide = true;
 			} else if (jour>=29 && mois == 2 || !bissextile) {
 					jourValide = false;
 			}
 			// début de la condition true/false 31 jours month
-		if(monthThirtyOne) {
+		if(bissextile) {
 			jourValide = true;
 		} else {
 			jourValide=false;
 		}
 		// début de la condition true/false 30 jours month
-			if(monthThirty)
+			if(!bissextile)
 			{
 				jourValide = true;
 			} else {
 				jourValide = false;
 			}
-			jourvalide = true;
-
 		// début de la condition true/false jourValide
 			if (jourValide) {
 				System.out.print("La date est valide !");
@@ -64,22 +67,20 @@ public class App {
 			}
 		}
 	public static void main(String[] args) {
-		int annee;
-		int mois;
-		int jour;
-		
+		int month, jour, annee;
 		Scanner sc = new Scanner(System.in);
 		
-		System.out.print("Inscrivez l'année : ");
-		annee = sc.nextInt();
-		System.out.print("Inscrivez le mois : ");
-		mois = sc.nextInt();
-		System.out.print("Inscrivez le jour : ");
+		System.out.println("Inscrivez le jour : ");
 		jour = sc.nextInt();
 		
-		sc.close();
+		System.out.println("Inscrivez le numéro du mois : ");
+		month = sc.nextInt(); // attribution d'une valeur à la variable 'month'
 		
-		dateValide(annee, mois, jour);
+		System.out.println("Inscrivez l'année");
+		annee = sc.nextInt();
+		
+		month(0,0);
+		dateValide(0,0,0);
 	}
 
 }
